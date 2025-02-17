721b9685b92db5b43a687492389a5874af7b2863
mukaweb <your-github-email@example.com>
2025-02-17 11:31:10 +0300

    tgfd

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'standalone',
-  // Enable CSS optimization in production
-  optimizeFonts: true,
-  swcMinify: true,
   // ... other config options
 };
 
